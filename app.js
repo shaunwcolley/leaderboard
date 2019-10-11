15c4beba23df345dba91fd9c41d5ac0efe973e1f
Shaun Colley <swc7838@gmail.com>
2019-10-10 22:34:01 -0500

    set up basic mustache-express files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mustacheExpress = require('mustache-express');
 require('dotenv').config();
 const app = express();
 
@@ -9,21 +10,23 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(bodyParser.json());
 
+app.engine('mustache', mustacheExpress());
+app.set('views','./views');
+app.set('view engine', 'mustache');
+
 const authenticate = require('./utils/authenticate');
 
 const keyGenerateRoutes = require('./routes/keyGenerate');
 
-app.use('/', keyGenerateRoutes)
+app.use('/', keyGenerateRoutes):
 
 app.get('/', (req, res) => {
-  res.json({'message': 'This is the leaderboard api. Go to /register to generate account'})
+  res.render('home')
 })
-
 
 app.get('/api/:key/scores', authenticate, (req,res) => {
   res.send('howdy')
 })
-
 
 app.listen(PORT, function() {
   console.log('Scores are being served...');
