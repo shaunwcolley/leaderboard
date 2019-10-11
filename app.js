81b42493a4c7011aa5a872d7127e0f340591a506
Shaun Colley <swc7838@gmail.com>
2019-10-11 10:36:57 -0500

    updated some styling and have general sense; now need to set up new schema for scores being inputted and registration for api keys

--- app.js
+++ app.js
@@ -2,23 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mustacheExpress = require('mustache-express');
+const path = require('path');
+
 require('dotenv').config();
+
 const app = express();
+const VIEWS_PATH = path.join(__dirname, '/views');
 
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.engine('mustache', mustacheExpress());
+app.engine('mustache', mustacheExpress(VIEWS_PATH + '/partials'));
 app.set('views','./views');
 app.set('view engine', 'mustache');
+app.use(express.static('public'))
 
 const authenticate = require('./utils/authenticate');
 
 const keyGenerateRoutes = require('./routes/keyGenerate');
 
-app.use('/', keyGenerateRoutes):
+app.use('/', keyGenerateRoutes);
 
 app.get('/', (req, res) => {
   res.render('home')
